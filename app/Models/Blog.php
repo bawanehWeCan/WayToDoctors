021dfbe215ced30255db247041b4f2a5213748ea
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-12-21 14:50:51 +0300

    FIX

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -22,7 +22,7 @@ class Blog extends Model
         }
     }
     public function categories(){
-        return $this->morphToMany( Category::class, 'categoryable' );
+        return $this->morphByMany( Category::class, 'categoryable' );
     }
 
 }
