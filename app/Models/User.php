37f67ffe44aabd9887c39f30f7acc735410ed43e
aya-matroud <ayamatroud8@gmail.com>
2023-01-02 06:15:02 -0800

    updateUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','phone','step','active'
+        'name', 'email', 'password','phone','step','active','question_number'
     ];
 
     /**
